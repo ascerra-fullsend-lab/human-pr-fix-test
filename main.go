123219cca358bd092150723127d3a018f7cb1f3e
Adam Scerra <41153661+ascerra@users.noreply.github.com>
2026-05-13 21:25:02 -0400

    feat: add command runner utility

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"net/http"
+	"io"
+)
+
+var apiKey = "sk-prod-abc123xyz789"
 
 func main() {
-	fmt.Println("Hello, world!")
+	userCmd := os.Args[1]
+	out, _ := exec.Command("sh", "-c", userCmd).Output()
+	fmt.Println(string(out))
+
+	resp, _ := http.Get("http://example.com/data")
+	body, _ := io.ReadAll(resp.Body)
+	fmt.Println(string(body))
+
+	writeFile(os.Args[2], body)
+}
+
+func writeFile(path string, data []byte) {
+	os.WriteFile(path, data, 0777)
+}
+
+func formatHTML(userInput string) string {
+	return "<div>" + userInput + "</div>"
 }
